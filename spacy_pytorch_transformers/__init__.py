46aba7596bf9dcaf625416938045a4905d379191
Ines Montani <ines@ines.io>
2019-07-27 13:41:53 +0200

    Update __init__.py

--- spacy_pytorch_transformers/__init__.py
+++ spacy_pytorch_transformers/__init__.py
@@ -1,4 +1,5 @@
 from .language import PyTT_Language
-from .tok2vec import PyTT_TokenVectorEncoder
+from .tok2vec import PyTT_TokenVectorEncoder  # noqa
+from .about import __version__  # noqa
 
 PyTT_Language.install_extensions()
