4025e0df3d87b53912fab2be01e1e950c1e5afa5
Ines Montani <ines@ines.io>
2019-07-29 20:39:28 +0200

    Add lang setting to init_model and add docs

--- examples/init_model.py
+++ examples/init_model.py
@@ -7,14 +7,15 @@ from spacy_pytorch_transformers import PyTT_TokenVectorEncoder
 @plac.annotations(
     path=("Output path", "positional", None, str),
     name=("Name of pre-trained model", "option", "n", str),
+    lang=("Language code to use", "option", "l", str),
 )
-def main(path, name="bert-base-uncased"):
-    nlp = PyTT_Language(pytt_name=name, meta={"lang": "en"})
+def main(path, name="bert-base-uncased", lang="en"):
+    nlp = PyTT_Language(pytt_name=name, meta={"lang": lang})
     nlp.add_pipe(nlp.create_pipe("sentencizer"))
     nlp.add_pipe(PyTT_WordPiecer.from_pretrained(nlp.vocab, name))
     nlp.add_pipe(PyTT_TokenVectorEncoder.from_pretrained(nlp.vocab, name))
     nlp.to_disk(path)
-    print(f"Saved '{name}' model to {path}")
+    print(f"Saved '{name}' ({lang}) model to {path}")
 
 
 if __name__ == "__main__":
