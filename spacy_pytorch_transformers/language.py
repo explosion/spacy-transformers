863cad94d27614d1f35e203e732a6d884c1d01dc
Matthew Honnibal <honnibal+gh@gmail.com>
2019-07-31 01:54:10 +0200

    Fix extension

--- spacy_pytorch_transformers/language.py
+++ spacy_pytorch_transformers/language.py
@@ -139,8 +139,7 @@ def get_wp_end(span):
 
 def get_span_wp_getter(attr):
     def span_getter(span):
-        return flatten_list([token._.get(attr) for token in span])
-
+        return [token._.get(attr) for token in span]
     return span_getter
 
 
