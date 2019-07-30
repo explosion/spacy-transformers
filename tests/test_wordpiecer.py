bb70962a4e9a4f8b934e124861201b255d8a3e36
Ines Montani <ines@ines.io>
2019-07-30 16:10:56 +0200

    Fix test

--- tests/test_wordpiecer.py
+++ tests/test_wordpiecer.py
@@ -1,5 +1,6 @@
 import pytest
 from spacy_pytorch_transformers import PyTT_WordPiecer
+from spacy_pytorch_transformers.util import is_special_token
 from spacy.vocab import Vocab
 from spacy.tokens import Doc
 
@@ -10,9 +11,9 @@ def wp(name):
 
 
 def test_wordpiecer(wp):
-    words = ["hello", "world"]
+    words = ["hello", "world", "this", "is", "a", "test"]
     doc = Doc(wp.vocab, words=words)
     doc[0].is_sent_start = True
     doc[1].is_sent_start = False
     doc = wp(doc)
-    assert doc._.pytt_word_pieces_ == words
+    assert [t for t in doc._.pytt_word_pieces_ if not is_special_token(t)] == words
