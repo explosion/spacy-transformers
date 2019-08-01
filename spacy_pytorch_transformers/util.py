4b39468e55f6e5078ebc94a383e2a77cfb46999a
Ines Montani <ines@ines.io>
2019-08-01 12:49:05 +0200

    Move alignment helpers to WordPiecer and improve flags

--- spacy_pytorch_transformers/util.py
+++ spacy_pytorch_transformers/util.py
@@ -3,22 +3,10 @@ from dataclasses import dataclass
 import pytorch_transformers as pytt
 from thinc.neural.ops import get_array_module
 from thinc.extra.wrappers import torch2xp
-import torch
-import re
 import numpy
 
 from . import _tokenizers
 
-
-# Flag to control whether to apply a fallback strategy when we don't align,
-# of making more aggressive replacements. It's not clear whether this will
-# lead to better or worse results than the ultimate fallback strategy,
-# of calling the sub-tokenizer on the spaCy tokens.
-# Probably trying harder to get alignment is good: the ultimate fallback
-# actually *changes what wordpieces we return*, so we get (potentially) different
-# results out of the transformer. The more aggressive alignment can only change
-# how we map those transformer features to tokens.
-FORCE_ALIGNMENT_WITH_REPLACEMENT = True
 
 Array = Union["numpy.ndarray", "cupy.ndarray"]
 Optimizer = Callable[[Array, Array, Optional[int]], None]
@@ -35,7 +23,6 @@ SPECIAL_TOKENS: Sequence[str] = (
     "<s>",
     "</s>",
 )
-alpha_re = re.compile(r"[^A-Za-z]+")
 
 
 @dataclass
@@ -48,7 +35,7 @@ class Activations:
 
     @classmethod
     def blank(cls, is_grad=False):
-        return cls(numpy.zeros((0,0), dtype="f"), [], [], [], is_grad=is_grad)
+        return cls(numpy.zeros((0, 0), dtype="f"), [], [], [], is_grad=is_grad)
 
     @classmethod
     def from_pytt(cls, fields, *, is_grad=False) -> "Activations":
@@ -103,8 +90,7 @@ class Activations:
         """Split into a list of Activation objects."""
         last_hiddens = ops.unflatten(self.lh, lengths)
         return [
-            Activations(lh, [], [], [], is_grad=self.is_grad)
-            for lh in last_hiddens
+            Activations(lh, [], [], [], is_grad=self.is_grad) for lh in last_hiddens
         ]
         # lh_values = [None] * len(shapes)
         # po_values = [None] * len(shapes)
@@ -188,83 +174,6 @@ def get_pytt_tokenizer(name):
         return _tokenizers.SerializableXLMTokenizer
     else:
         raise ValueError(f"Unsupported PyTT config name: '{name}'")
-
-
-def align_word_pieces(spacy_tokens, wp_tokens, specials=SPECIAL_TOKENS):
-    """Align tokens against word-piece tokens. The alignment is returned as a
-    list of lists. If alignment[3] == [4, 5, 6], that means that spacy_tokens[3]
-    aligns against 3 tokens: wp_tokens[4], wp_tokens[5] and wp_tokens[6].
-    All spaCy tokens must align against at least one element of wp_tokens.
-    """
-    spacy_tokens = list(spacy_tokens)
-    wp_tokens = list(wp_tokens)
-    offset = 0
-    while wp_tokens and wp_tokens[0] in specials:
-        wp_tokens.pop(0)
-        offset += 1
-    while wp_tokens and wp_tokens[-1] in specials:
-        wp_tokens.pop(-1)
-    if not wp_tokens:
-        return [[] for _ in spacy_tokens]
-    elif not spacy_tokens:
-        return []
-    # Check alignment
-    if "".join(spacy_tokens).lower() != "".join(wp_tokens).lower():
-        if FORCE_ALIGNMENT_WITH_AGGRESSIVE_REPLACEMENT:
-            # Force alignment
-            spacy_tokens = [alpha_re.sub("", t) for t in spacy_tokens]
-            wp_tokens = [alpha_re.sub("", t) for t in wp_tokens]
-            spacy_string = "".join(spacy_tokens).lower()
-            wp_string = "".join(wp_tokens).lower()
-            if spacy_string == wp_string:
-                return _align(spacy_tokens, wp_tokens, offset)
-        # If either we're not trying the fallback alignment, or the fallback
-        # fails, we return None. This tells the wordpiecer to align by
-        # calling the sub-tokenizer on the spaCy tokens.
-        return None
-    output = _align(spacy_tokens, wp_tokens, offset)
-    return output
-
-
-def _align(seq1, seq2, offset):
-    # Map character positions to tokens
-    map1 = _get_char_map(seq1)
-    map2 = _get_char_map(seq2)
-    # For each token in seq1, get the set of tokens in seq2
-    # that share at least one character with that token.
-    alignment = [set() for _ in seq1]
-    unaligned = set(range(len(seq2)))
-    for char_position in range(map1.shape[0]):
-        i = map1[char_position]
-        j = map2[char_position]
-        alignment[i].add(j)
-        if j in unaligned:
-            unaligned.remove(j)
-    # Sort, make list
-    output = [sorted(list(s)) for s in alignment]
-    # Expand alignment to adjacent unaligned tokens of seq2
-    for indices in output:
-        if indices:
-            while indices[0] >= 1 and indices[0] - 1 in unaligned:
-                indices.insert(0, indices[0] - 1)
-            last = len(seq2) - 1
-            while indices[-1] < last and indices[-1] + 1 in unaligned:
-                indices.append(indices[-1] + 1)
-    # Add offset
-    for indices in output:
-        for i in range(len(indices)):
-            indices[i] += offset
-    return output
-
-
-def _get_char_map(seq):
-    char_map = numpy.zeros((sum(len(token) for token in seq),), dtype="i")
-    offset = 0
-    for i, token in enumerate(seq):
-        for j in range(len(token)):
-            char_map[offset + j] = i
-        offset += len(token)
-    return char_map
 
 
 def pad_batch(batch: List[Array], xp=numpy) -> Array:
--- spacy_pytorch_transformers/wordpiecer.py
+++ spacy_pytorch_transformers/wordpiecer.py
@@ -1,7 +1,9 @@
 from spacy.pipeline import Pipe
 from spacy.util import minibatch
+import re
+import numpy
 
-from .util import get_pytt_tokenizer, align_word_pieces, flatten_list
+from .util import get_pytt_tokenizer, flatten_list, SPECIAL_TOKENS
 
 
 class PyTT_WordPiecer(Pipe):
@@ -18,9 +20,9 @@ class PyTT_WordPiecer(Pipe):
         return cls(nlp.vocab, **cfg)
 
     @classmethod
-    def from_pretrained(cls, vocab, pytt_name):
+    def from_pretrained(cls, vocab, pytt_name, **cfg):
         model = get_pytt_tokenizer(pytt_name).from_pretrained(pytt_name)
-        return cls(vocab, model=model, pytt_name=pytt_name)
+        return cls(vocab, model=model, pytt_name=pytt_name, **cfg)
 
     @classmethod
     def Model(cls, pytt_name, **kwargs):
@@ -48,6 +50,15 @@ class PyTT_WordPiecer(Pipe):
         scores = self.predict([doc])
         self.set_annotations([doc], scores)
         return doc
+
+    @property
+    def alignment_strategy(self):
+        """Mostly used for debugging to make the WordPiecer raise error on
+        misaligned tokens and prevent forcing alignment.
+        """
+        retry_alignment = self.cfg.get("retry_alignment", True)
+        force_alignment = self.cfg.get("force_alignment", True)
+        return (retry_alignment, force_alignment)
 
     def pipe(self, stream, batch_size=128):
         """Process Doc objects as a stream and assign the extracted features.
@@ -87,6 +98,7 @@ class PyTT_WordPiecer(Pipe):
         docs (iterable): A batch of `Doc` objects.
         outputs (iterable): A batch of outputs.
         """
+        retry, force = self.alignment_strategy
         for doc, output in zip(docs, outputs):
             offset = 0
             doc_word_pieces = []
@@ -96,13 +108,20 @@ class PyTT_WordPiecer(Pipe):
                 spacy_tokens = [self.model.clean_token(w.text) for w in sent]
                 new_wp_tokens = [self.model.clean_wp_token(t) for t in wp_tokens]
                 assert len(wp_tokens) == len(new_wp_tokens)
-                sent_align = align_word_pieces(spacy_tokens, new_wp_tokens)
+                sent_align = align_word_pieces(spacy_tokens, new_wp_tokens, retry=retry)
                 if sent_align is None:
-                    print("Alignment failed. Using fallback")
+                    if not force:
+                        spacy_string = "".join(spacy_tokens).lower()
+                        wp_string = "".join(new_wp_tokens).lower()
+                        print("spaCy:", spacy_string)
+                        print("WP:", wp_string)
+                        raise AssertionError((spacy_string, wp_string))
                     # As a final fallback, we resort to word-piece tokenizing
                     # the spaCy tokens individually, to make the alignment
                     # trivial.
-                    wp_tokens, sent_align = _tokenize_individual_tokens(self.model, sent)
+                    wp_tokens, sent_align = _tokenize_individual_tokens(
+                        self.model, sent
+                    )
                 # We need to align into the flattened document list, instead
                 # of just into this sentence. So offset by number of wp tokens.
                 for token_align in sent_align:
@@ -130,6 +149,100 @@ class PyTT_WordPiecer(Pipe):
             doc._.pytt_word_pieces = doc_word_piece_ids
             doc._.pytt_word_pieces_ = doc_word_pieces
             doc._.pytt_alignment = doc_alignment
+
+
+alpha_re = re.compile(r"[^A-Za-z]+")
+
+
+def align_word_pieces(spacy_tokens, wp_tokens, specials=SPECIAL_TOKENS, retry=True):
+    """Align tokens against word-piece tokens. The alignment is returned as a
+    list of lists. If alignment[3] == [4, 5, 6], that means that spacy_tokens[3]
+    aligns against 3 tokens: wp_tokens[4], wp_tokens[5] and wp_tokens[6].
+    All spaCy tokens must align against at least one element of wp_tokens.
+    """
+    spacy_tokens = list(spacy_tokens)
+    wp_tokens = list(wp_tokens)
+    offset = 0
+    while wp_tokens and wp_tokens[0] in specials:
+        wp_tokens.pop(0)
+        offset += 1
+    while wp_tokens and wp_tokens[-1] in specials:
+        wp_tokens.pop(-1)
+    if not wp_tokens:
+        return [[] for _ in spacy_tokens]
+    elif not spacy_tokens:
+        return []
+    # Check alignment
+    spacy_string = "".join(spacy_tokens).lower()
+    wp_string = "".join(wp_tokens).lower()
+    if spacy_string != wp_string:
+        if retry:
+            # Flag to control whether to apply a fallback strategy when we
+            # don't align, of making more aggressive replacements. It's not
+            # clear whether this will lead to better or worse results than the
+            # ultimate fallback strategy, of calling the sub-tokenizer on the
+            # spaCy tokens. Probably trying harder to get alignment is good:
+            # the ultimate fallback actually *changes what wordpieces we
+            # return*, so we get (potentially) different results out of the
+            # transformer. The more aggressive alignment can only change how we
+            # map those transformer features to tokens.
+            spacy_tokens = [alpha_re.sub("", t) for t in spacy_tokens]
+            wp_tokens = [alpha_re.sub("", t) for t in wp_tokens]
+            spacy_string = "".join(spacy_tokens).lower()
+            wp_string = "".join(wp_tokens).lower()
+            if spacy_string == wp_string:
+                return _align(spacy_tokens, wp_tokens, offset)
+        else:
+            print("spaCy:", spacy_string)
+            print("WP:", wp_string)
+            raise AssertionError((spacy_string, wp_string))
+        # If either we're not trying the fallback alignment, or the fallback
+        # fails, we return None. This tells the wordpiecer to align by
+        # calling the sub-tokenizer on the spaCy tokens.
+        return None
+    output = _align(spacy_tokens, wp_tokens, offset)
+    return output
+
+
+def _align(seq1, seq2, offset):
+    # Map character positions to tokens
+    map1 = _get_char_map(seq1)
+    map2 = _get_char_map(seq2)
+    # For each token in seq1, get the set of tokens in seq2
+    # that share at least one character with that token.
+    alignment = [set() for _ in seq1]
+    unaligned = set(range(len(seq2)))
+    for char_position in range(map1.shape[0]):
+        i = map1[char_position]
+        j = map2[char_position]
+        alignment[i].add(j)
+        if j in unaligned:
+            unaligned.remove(j)
+    # Sort, make list
+    output = [sorted(list(s)) for s in alignment]
+    # Expand alignment to adjacent unaligned tokens of seq2
+    for indices in output:
+        if indices:
+            while indices[0] >= 1 and indices[0] - 1 in unaligned:
+                indices.insert(0, indices[0] - 1)
+            last = len(seq2) - 1
+            while indices[-1] < last and indices[-1] + 1 in unaligned:
+                indices.append(indices[-1] + 1)
+    # Add offset
+    for indices in output:
+        for i in range(len(indices)):
+            indices[i] += offset
+    return output
+
+
+def _get_char_map(seq):
+    char_map = numpy.zeros((sum(len(token) for token in seq),), dtype="i")
+    offset = 0
+    for i, token in enumerate(seq):
+        for j in range(len(token)):
+            char_map[offset + j] = i
+        offset += len(token)
+    return char_map
 
 
 def _tokenize_individual_tokens(model, sent):
