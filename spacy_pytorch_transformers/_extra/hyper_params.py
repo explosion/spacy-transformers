1b0491b03404ef144e5da9ef6f93f42cb6439d6b
Matthew Honnibal <honnibal+gh@gmail.com>
2019-08-04 21:35:16 +0200

    Add hyper_params module

---
+++ spacy_pytorch_transformers/_extra/hyper_params.py
@@ -0,0 +1,32 @@
+from configparser import ConfigParser
+
+
+FIELDS = dict(
+    max_seq_length=int,
+    dropout=float,
+    batch_size=int,
+    eval_batch_size=int,
+    learning_rate=float,
+    weight_decay=float,
+    adam_epsilon=float,
+    max_grad_norm=float,
+    num_train_epochs=int,
+    max_steps=int,
+    warmup_steps=int,
+    seed=int
+)
+
+
+class HyperParams:
+    pass
+
+
+def get_hyper_params(path):
+    cfg = ConfigParser()
+    with path.open("r", encoding="utf8") as file_:
+        cfg.read_string(file_.read())
+    values = cfg["defaults"]
+    HP = HyperParams()
+    for field, type_ in FIELDS.items():
+        setattr(HP, field, type_(values[field]))
+    return HP
