49c4f1c33c277a1bda472c7253ab9fe4b7b50375
Ines Montani <ines@ines.io>
2019-08-09 20:53:32 +0200

    Increment version [ci skip]

--- spacy_pytorch_transformers/about.py
+++ spacy_pytorch_transformers/about.py
@@ -1,5 +1,5 @@
 __title__ = "spacy-pytorch-transformers"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __summary__ = "spaCy pipelines for pre-trained BERT and other transformers"
 __uri__ = "https://explosion.ai"
 __author__ = "Explosion"
