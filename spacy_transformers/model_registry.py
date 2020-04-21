38f78fff4f5a53c91198018ff015bfe6970aa726
Matthew Honnibal <honnibal+gh@gmail.com>
2020-04-21 15:01:58 +0200

    Update model registry

--- spacy_transformers/model_registry.py
+++ spacy_transformers/model_registry.py
@@ -1,11 +1,33 @@
 from typing import Tuple, Callable, List, Optional
 
 from spacy.util import registry
-from spacy.pipeline.tok2vec import Tok2VecListener
+from thinc.api import PyTorchWrapper, Model, torch2xp, xp2torch
+from .types import TransformerOutput, TokensPlus
+
+
+@thinc.registry.layers("transformers_tokenizer.v1")
+def TransformersTokenizer(name: str) -> Model[List[List[str]], TokensPlus]:
+    def forward(
+        model, texts: List[List[str]], is_train: bool
+    ) -> Tuple[TokensPlus, Callable]:
+        tokenizer = model.attrs["tokenizer"]
+        token_data = tokenizer.batch_encode_plus(
+            [(text, None) for text in texts],
+            add_special_tokens=True,
+            return_token_type_ids=True,
+            return_attention_masks=True,
+            return_input_lengths=True,
+            return_tensors="pt",
+        )
+        return TokensPlus(**token_data), lambda d_tokens: []
+
+    return Model(
+        "tokenizer", forward, attrs={"tokenizer": AutoTokenizer.from_pretrained(name)},
+    )
 
 
 @registry.architectures.register("spacy.Transformer.v1")
-def Transformer(name: str) -> Model[TokensPlus, List[Floats2d]]:
+def Transformer(name: str) -> Model[TokensPlus, TransformerOutput]:
     return PyTorchWrapper(
         AutoModel.from_pretrained(name),
         convert_inputs=convert_transformer_inputs,
@@ -22,23 +44,14 @@ def convert_transformer_inputs(model, tokens: TokensPlus, is_train):
     return ArgsKwargs(args=(), kwargs=kwargs), lambda dX: []
 
 
-def convert_transformer_outputs(model, inputs_outputs, is_train):
-    layer_inputs, torch_outputs = inputs_outputs
-    torch_tokvecs: torch.Tensor = torch_outputs[0]
-    # Free the memory as soon as we can
-    torch_outputs = None
-    lengths = list(layer_inputs.input_len)
-    tokvecs: List[Floats2d] = model.ops.unpad(torch2xp(torch_tokvecs), lengths)
-    # Remove the BOS and EOS markers.
-    tokvecs = [arr[1:-1] for arr in tokvecs]
+def convert_transformer_outputs(model, tokens_tensors, is_train):
+    tokens, tensors = tokens_tensors
+    output = TransformerOutput(tokens, [torch2xp(tensor) for tensor in tensors])
 
-    def backprop(d_tokvecs: List[Floats2d]) -> ArgsKwargs:
-        # Restore entries for bos and eos markers.
-        row = model.ops.alloc2f(1, d_tokvecs[0].shape[1])
-        d_tokvecs = [model.ops.xp.vstack((row, arr, row)) for arr in d_tokvecs]
+    def backprop(d_output: TransformerOutput) -> ArgsKwargs:
         return ArgsKwargs(
-            args=(torch_tokvecs,),
-            kwargs={"grad_tensors": xp2torch(model.ops.pad(d_tokvecs))},
+            args=(tensors,),
+            kwargs={"grad_tensors": [xp2torch(x) for x in TransformerOutput.tensors]}
         )
 
-    return tokvecs, backprop
+    return output, backprop
