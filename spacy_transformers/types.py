1606a82e5344d45fd529689b94e35fa8a6a95357
Matthew Honnibal <honnibal+gh@gmail.com>
2020-04-21 14:54:47 +0200

    Add some types

---
+++ spacy_transformers/types.py
@@ -0,0 +1,22 @@
+from typing import Optional, Tuple, List
+from dataclasses import dataclass
+import torch
+
+
+@dataclass
+class TokensPlus:
+    """Dataclass to hold the output of the Huggingface 'batch_encode_plus' method."""
+
+    input_ids: torch.Tensor
+    token_type_ids: torch.Tensor
+    attention_mask: torch.Tensor
+    input_len: List[int]
+    overflowing_tokens: Optional[torch.Tensor] = None
+    num_truncated_tokens: Optional[torch.Tensor] = None
+    special_tokens_mask: Optional[torch.Tensor] = None
+
+
+@dataclass
+class TransformerOutput:
+    tokens: TokensPlus
+    tensors: Tuple[torch.Tensor]
