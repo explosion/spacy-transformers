95282fdb755320924c21409688f1d77c21fe64bb
Matthw Honnibal <honnibal+gh@gmail.com>
2020-05-10 15:07:35 +0200

    Start testing alignment

--- spacy_transformers/tests/test_alignment.py
+++ spacy_transformers/tests/test_alignment.py
@@ -3,88 +3,57 @@ from dataclasses import dataclass
 from spacy.tokens import Doc
 from spacy.vocab import Vocab
 import pytest
-from .._align import align_offsets, align_spans, align_docs
+from .._align import BatchAlignment
 
 
-@dataclass
 class AlignmentCase:
-    string: str
-    offsets1: List[Optional[Tuple[int, int]]]
-    offsets2: List[Optional[Tuple[int, int]]]
-    a2b: List[List[int]]
-    b2a: List[List[int]]
+    A: List[List[str]]
+    B: List[List[str]]
+    a2b: List[int]
+    b2a: List[int]
 
-    @property
-    def words1(self):
-        words = []
-        for entry in self.offsets1:
-            if entry is not None:
-                start, end = entry
-                words.append(self.string[start:end])
-        return words
-
-    def make_doc(self):
-        return Doc(Vocab(), words=self.words1, spaces=[False] * len(self.words1))
-
-    def make_spans(self):
-        doc = self.make_doc()
-        return [doc[:]]
 
 
 CASES = {
-    "match": AlignmentCase(
-        "ab", [(0, 1), (1, 2)], [(0, 1), (1, 2)], [[0], [1]], [[0], [1]]
-    ),
-    "multi1": AlignmentCase("ab", [(0, 2)], [(0, 1), (1, 2)], [[0, 1]], [[0], [0]]),
-    "multi2": AlignmentCase("ab", [(0, 1), (1, 2)], [(0, 2)], [[0], [0]], [[0, 1]]),
-    "mix1": AlignmentCase(
-        "abc",
-        [(0, 2), (2, 3)],
-        [(0, 1), (1, 2), (2, 3)],
-        [[0, 1], [2]],
-        [[0], [0], [1]],
-    ),
-    "nones": AlignmentCase(
-        "abc",
-        [None, (0, 2), (2, 3), None],
-        [(0, 1), (1, 2), (2, 3)],
-        [[], [0, 1], [2], []],
-        [[1], [1], [2]],
-    ),
+    "match": AlignmentCase([["a", "b"]], [["a", "b"]], [0, 1], [0, 1]),
+    "multi1": AlignmentCase([["a", "b"]], [["ab"]], , [0, 0], [0]),
+    "multi2": AlignmentCase([["ab"]], [["a", "b"]], [0], [0, 0]),
+    "mix1": AlignmentCase([["ab", "c"]], [["a", "bc"]], [0, 1], [0, 1])
 }
 
 
-@pytest.mark.parametrize("case_id", list(CASES.keys()))
-def test_align_offsets(case_id):
-    case = CASES[case_id]
-    predicted_a2b = align_offsets(case.offsets1, case.offsets2)
-    assert predicted_a2b == case.a2b
-    predicted_b2a = align_offsets(case.offsets2, case.offsets1)
-    assert predicted_b2a == case.b2a
 
 
-@pytest.mark.parametrize("case_id", list(CASES.keys()))
-def test_align_spans(case_id):
-    # This is a pretty bad test, it doesn't check any of the likely logic errors.
-    # But at least it does run the code?
-    case = CASES[case_id]
-    spans = case.make_spans()
-    alignments = align_spans(spans, [case.offsets2])
-    assert len(alignments) == len(spans)
-    for i, span in enumerate(spans):
-        assert len(alignments[i]) == len(span)
-        for align in alignments[i]:
-            for entry in align:
-                assert entry[0] == i
+words1 = ["a", "b"]
+words2 = ["ab"]
+
+feats1 = [
+    [1, -1, 1],
+    [0, 0, 0]
+]
+
+feats2 = [
+    [0.5, -0.5, 0.5]
+]
+
+alignment = [0, 1]
+
+for i, j in enumerate(alignment):
+    feats2[j] += feats1[i]
 
 
-@pytest.mark.parametrize("case_id", list(CASES.keys()))
-def test_align_docs(case_id):
-    # This is a pretty bad test, it doesn't check any of the likely logic errors.
-    # But at least it does run the code?
-    case = CASES[case_id]
-    spans = case.make_spans()
-    doc_alignments = align_docs(spans, [case.offsets2])
-    assert len(doc_alignments) == 1
-    span_alignments = align_spans(spans, [case.offsets2])
-    assert doc_alignments == span_alignments
+words1 = ["ab"]
+words2 = ["a", "b"]
+
+feats1 = [
+    [1, -1, 1],
+]
+
+feats2 = [
+    [1, -1, 1],
+    [1, -1, 1],
+]
+
+alignment = [0, 1]
+
+
