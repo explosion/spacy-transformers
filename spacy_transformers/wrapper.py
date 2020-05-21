75c04552e088eb0541b7ce572a11fe2b96885a4f
Matthew Honnibal <honnibal+gh@gmail.com>
2020-05-21 15:44:02 +0200

    Refactor

--- spacy_transformers/wrapper.py
+++ spacy_transformers/wrapper.py
@@ -76,29 +76,4 @@ def forward(
     return output, backprop_transformer
 
 
-def PyTorchTransformer(transformer):
-    return PyTorchWrapper(
-        transformer,  # e.g. via AutoModel.from_pretrained(name),
-        convert_inputs=_convert_transformer_inputs,
-        convert_outputs=_convert_transformer_outputs,
-    )
 
-
-def _convert_transformer_inputs(model, tokens: BatchEncoding, is_train):
-    # Adapter for the PyTorchWrapper. See https://thinc.ai/docs/usage-frameworks
-    kwargs = {
-        "input_ids": tokens["input_ids"],
-        "attention_mask": tokens["attention_mask"],
-    }
-    if "token_type_ids" in tokens:
-        kwargs["token_type_ids"] = tokens["token_type_ids"]
-    return ArgsKwargs(args=(), kwargs=kwargs), lambda dX: []
-
-
-def _convert_transformer_outputs(model, inputs_outputs, is_train):
-    _, tensors = inputs_outputs
-
-    def backprop(d_tensors: List[torch.Tensor]) -> ArgsKwargs:
-        return ArgsKwargs(args=(tensors,), kwargs={"grad_tensors": d_tensors})
-
-    return tensors, backprop
