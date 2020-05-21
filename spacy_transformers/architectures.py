ae6cd8c621b463442458ffc893ec648f3c9bd66e
Matthew Honnibal <honnibal+gh@gmail.com>
2020-05-21 16:25:49 +0200

    Fix flake8

--- spacy_transformers/architectures.py
+++ spacy_transformers/architectures.py
@@ -1,7 +1,11 @@
 from typing import List, Callable, Union
-from thinc.api import Model, chain
+from pathlib import Path
+from transformers import AutoModel, AutoTokenizer
+from thinc.api import Model, chain, CupyOps
 from thinc.types import Ragged, Floats2d
-from .layers import TransformerListener, trfs2arrays, split_trf_batch
+from spacy.tokens import Doc
+from .layers import TransformerModel, TransformerListener
+from .layers import trfs2arrays, split_trf_batch
 from .data_classes import TransformerData
 from .util import registry
 
@@ -21,6 +25,7 @@ def transformer_tok2vec_v1(
     transformer,
     pooling,
     get_spans,
+    width: int,
     grad_factor: float = 1.0,
 ) -> Model[List[TransformerData], List[Floats2d]]:
     return chain(
@@ -30,27 +35,13 @@ def transformer_tok2vec_v1(
     )
 
 
-@registry.architectures.register("spacy-transformers.empty_transformer.v1")
-def empty_transformer_v1(get_spans: Callable, config) -> Model[List[Doc], TransformerData]:
-    return Model(
-        "transformer",
-        forward,
-        layers=[],
-        attrs={
-            "tokenizer": None,
-            "get_spans": get_spans,
-            "load": partial(load_transformer, get_spans=get_spans, config=config)
-        },
-        dims={"nO": None},
-    )
-
-
 @registry.architectures.register("spacy-transformers.load_transformer.v1")
 def load_transformer_v1(
     load_from: Union[str, Path], get_spans: Callable, config: dict
 ) -> Model[List[Doc], TransformerData]:
     transformer = AutoModel.from_pretrained(load_from)
     tokenizer = AutoTokenizer.from_pretrained(load_from, **config)
+    model = TransformerModel(transformer, tokenizer, get_spans)
     if isinstance(model.ops, CupyOps):
         transformer.cuda()
-    return TransformerModel(transformer, tokenizer, get_spans)
+    return model
