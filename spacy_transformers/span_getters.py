be2cde90e39398cbaac72a14bd90e9e42346eae6
Adriane Boyd <adrianeboyd@gmail.com>
2021-07-21 09:11:12 +0200

    Enable pickling of span getters and annotation setters
    Required for multiprocessing with spawn.

--- spacy_transformers/span_getters.py
+++ spacy_transformers/span_getters.py
@@ -1,9 +1,28 @@
 from typing import Callable, Iterable, List
+from functools import partial
 from spacy.tokens import Doc, Span
 
 from .util import registry
 
 SpannerT = Callable[[List[Doc]], List[List[Span]]]
+
+
+def get_strided_spans(
+    docs: Iterable[Doc], window: int, stride: int
+) -> List[List[Span]]:
+    spans = []
+    for doc in docs:
+        start = 0
+        spans.append([])
+        for i in range(len(doc) // stride):
+            spans[-1].append(doc[start : start + window])
+            if (start + window) >= len(doc):
+                break
+            start += stride
+        else:
+            if start < len(doc):
+                spans[-1].append(doc[start:])
+    return spans
 
 
 @registry.span_getters("spacy-transformers.strided_spans.v1")
@@ -13,26 +32,14 @@ def configure_strided_spans(window: int, stride: int) -> SpannerT:
 
     If you set the window and stride to the same value, the spans will cover
     each token once. Setting 'stride' lower than 'window' will allow for an
-    overlap, so that some tokens are counted twice. This can be desirable, 
+    overlap, so that some tokens are counted twice. This can be desirable,
     because it allows all tokens to have both a left and right context.
     """
+    return partial(get_strided_spans, window=window, stride=stride)
 
-    def get_strided_spans(docs: Iterable[Doc]) -> List[List[Span]]:
-        spans = []
-        for doc in docs:
-            start = 0
-            spans.append([])
-            for i in range(len(doc) // stride):
-                spans[-1].append(doc[start : start + window])
-                if (start + window) >= len(doc):
-                    break
-                start += stride
-            else:
-                if start < len(doc):
-                    spans[-1].append(doc[start:])
-        return spans
 
-    return get_strided_spans
+def get_sent_spans(docs: Iterable[Doc]) -> List[List[Span]]:
+    return [list(doc.sents) for doc in docs]
 
 
 @registry.span_getters("spacy-transformers.sent_spans.v1")
@@ -43,11 +50,11 @@ def configure_get_sent_spans() -> Callable:
     in somewhat uneven batches, depending on the sentence lengths. However,
     it does provide the transformer with more meaningful windows to attend over.
     """
-
-    def get_sent_spans(docs: Iterable[Doc]) -> List[List[Span]]:
-        return [list(doc.sents) for doc in docs]
-
     return get_sent_spans
+
+
+def get_doc_spans(docs: Iterable[Doc]) -> List[List[Span]]:
+    return [[doc[:]] for doc in docs]
 
 
 @registry.span_getters("spacy-transformers.doc_spans.v1")
@@ -57,10 +64,6 @@ def configure_get_doc_spans() -> Callable:
     the best approach if your `Doc` objects already refer to relatively short
     texts.
     """
-
-    def get_doc_spans(docs: Iterable[Doc]) -> List[List[Span]]:
-        return [[doc[:]] for doc in docs]
-
     return get_doc_spans
 
 
