057ef19b73d4cfbc4949e0b34cefcc38c092f19e
svlandeg <sofie.vanlandeghem@gmail.com>
2021-07-22 12:38:41 +0200

    add and correct tests

--- spacy_transformers/tests/test_serialize.py
+++ spacy_transformers/tests/test_serialize.py
@@ -1,4 +1,5 @@
 from spacy import Language
+from spacy.util import make_tempdir
 
 from spacy_transformers import TransformerData
 import srsly
@@ -29,5 +30,38 @@ def test_transformer_model_tobytes():
 
     nlp2 = Language()
     trf2 = nlp2.add_pipe("transformer")
-    nlp2.initialize()
     trf2.from_bytes(trf_bytes)
+
+
+def test_transformer_model_todisk():
+    nlp = Language()
+    trf = nlp.add_pipe("transformer")
+    nlp.initialize()
+    with make_tempdir() as d:
+        trf.to_disk(d)
+        nlp2 = Language()
+        trf2 = nlp2.add_pipe("transformer")
+        trf2.from_disk(d)
+
+
+def test_transformer_pipeline_tobytes():
+    nlp = Language()
+    nlp.add_pipe("transformer")
+    nlp.initialize()
+    assert nlp.pipe_names == ["transformer"]
+    nlp_bytes = nlp.to_bytes()
+
+    nlp2 = Language()
+    nlp2.add_pipe("transformer")
+    nlp2.from_bytes(nlp_bytes)
+    assert nlp2.pipe_names == ["transformer"]
+
+
+def test_transformer_pipeline_todisk():
+    nlp = Language()
+    nlp.add_pipe("transformer")
+    nlp.initialize()
+    with make_tempdir() as d:
+        nlp.to_disk(d)
+        nlp2 = Language()
+        nlp2.from_disk(d)
