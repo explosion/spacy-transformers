669546060a1568600762d8dac02909ef35d22a65
svlandeg <sofie.vanlandeghem@gmail.com>
2021-08-09 19:21:19 +0200

    use distilbert instead of alberta

--- spacy_transformers/tests/test_serialize.py
+++ spacy_transformers/tests/test_serialize.py
@@ -88,7 +88,7 @@ inline_cfg_string = """
 
     [components.tagger.model.tok2vec]
     @architectures = "spacy-transformers.Tok2VecTransformer.v1"
-    name = "albert-base-v2"
+    name = "distilbert-base-uncased"
     tokenizer_config = {"use_fast": true}
     grad_factor = 1.0
 
