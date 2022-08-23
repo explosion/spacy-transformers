a2fa5ac328dd4bcaea49d915bf768b490337203c
Adriane Boyd <adrianeboyd@gmail.com>
2022-08-23 18:56:54 +0200

    Fix types

--- spacy_transformers/align.pyi
+++ spacy_transformers/align.pyi
@@ -1,20 +1,22 @@
-from typing import List, Tuple, Callable, Optional
+from typing import Dict, List, Tuple, Callable, Optional
 from spacy.tokens import Span, Token
 from thinc.api import Ops
 from thinc.types import Ragged, Floats2d
 
-
-def apply_alignment(ops: Ops, align: Ragged, X: Floats2d) -> Tuple[Ragged, Callable]: ...
-
+def apply_alignment(
+    ops: Ops, align: Ragged, X: Floats2d
+) -> Tuple[Ragged, Callable]: ...
 def get_token_positions(spans: List[Span]) -> Dict[Token, int]: ...
-
 def get_alignment_via_offset_mapping(
     spans: List[Span],
     offset_mapping: List[Tuple[int]],
 ) -> Ragged: ...
-
 def get_alignment(
     spans: List[Span],
     wordpieces: List[List[str]],
     special_tokens: Optional[List[str]] = None,
 ) -> Ragged: ...
+def get_span2wp_from_offset_mapping(
+    span: Span,
+    wp_char_offsets: Tuple[int],
+) -> List[List[int]]: ...
